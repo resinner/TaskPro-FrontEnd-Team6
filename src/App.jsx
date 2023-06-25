cd08a1d4eb6c2dd765837083bcef35fd0d66273f
resinner <revengingsinner@gmail.com>
2023-06-25 12:53:48 +0300

    install  react-datepicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
+import { Layout } from "components/Layout/Layout"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+TaskPro
+    </Layout>
   );
 };
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,52 @@
 import styled from 'styled-components';
 
-export const Layout = styled.div`
-padding: 16px;
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
   }
 `;
 
+
+export const Header = styled.header`
+  position: fixed;
+  top: 0;
+  z-index: 10;
+  width: 100%;
+  background-color: #161616;
+
+  @media screen and (min-width: 375px) {
+    width: 375px;
+    height: 60px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+    height: 68px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+
+export const Layout = ({ children }) => {
+  return (
+    <Container>
+      <Header>Header</Header>
+      <main>{ children } This is main</main>
+      <footer>Footer</footer>
+    </Container>
+  )
+}
