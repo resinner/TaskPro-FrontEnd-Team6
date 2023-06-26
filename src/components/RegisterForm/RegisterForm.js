31efd33d83ff21fb16efc9d5385b69e5b0628679
Natalia Moskvychova <natashamoskv@gmail.com>
2023-06-26 22:31:08 +0300

    add redux for auth

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import { AuthForm, AuthFormWrapper, AuthFormPasswordIcon, AuthFormField, AuthFormSubmitButton } from 'pages/AuthPage/AuthPageStyles';
+import { AuthForm, AuthFormWrapper, AuthFormPasswordIcon, AuthFormField, AuthFormSubmitButton, ErrorSection } from 'pages/AuthPage/AuthPageStyles';
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
-import { Formik, ErrorMessage } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
-// import { register } from '../../redux/auth/operations';
+import { register } from 'redux/auth/authOperations';
 
 const validationSchema = Yup.object().shape({
     name: Yup.string().min(2, 'Name must be at least 2 characters').max(32, 'Name must be at most 32 characters').required('Name is required'),
     email: Yup.string().email('Invalid email').required('Email is required'),
     password: Yup.string().trim().required('Password is required')
         .min(8, 'Password must be at least 8 characters').max(64, 'Password must be at most 64 characters')
-        .matches( /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$!%*?&]+$/, 'Password must contain at least one uppercase letter, and one lowercase letter'
+        .matches( /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]+$/, 'Password must contain at least one letter and at least one number'
     ),
 })
 
@@ -26,18 +26,17 @@ const initialValues = {
 
 export default function RegisterForm() {
     const [showPassword, setShowPassword] = useState(false);
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     
     const handleTogglePassword = () => {
         setShowPassword(!showPassword);
     }
 
     const onSubmit = (values,  { resetForm }) => {
-        // const { name, email, password } = values;
-        console.log(values)
-        // dispatch(
-        //     register({ name, email, password})
-        // );
+        const { name, email, password } = values;
+        dispatch(
+            register({ name, email, password})
+        );
         resetForm();
     }
 
@@ -45,17 +44,17 @@ export default function RegisterForm() {
         <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
             <AuthForm>
                 <AuthFormWrapper>
-                    <ErrorMessage name="name" component="div" className='auth-form__error-message' />
+                    <ErrorSection name="name" component="div" />
                     <AuthFormField type="text" id="name" name="name" placeholder='Enter your name' />
                 </AuthFormWrapper>
 
                 <AuthFormWrapper>
-                    <ErrorMessage name="email" component="div" className='auth-form__error-message' />
+                    <ErrorSection name="email" component="div" />
                     <AuthFormField type="email" id="email" name="email" placeholder='Enter your email' />
                 </AuthFormWrapper>
 
                 <AuthFormWrapper>
-                    <ErrorMessage name="password" component="div" className='auth-form__error-message' />
+                    <ErrorSection name="password" component="div" />
                     <AuthFormField type={showPassword ? 'text' : 'password'} id="password" name="password" placeholder='Create a password' />
                     <AuthFormPasswordIcon onClick={handleTogglePassword}>
                         {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,34 @@
+import { register, logIn, logOut } from "./authOperations";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: { name: null, email: null },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    extraReducers: {
+        [register.fulfilled](state, action) {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+            state.isLoggedIn = true;
+        },
+        [logIn.fulfilled](state, action) {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+            state.isLoggedIn = true;
+        },
+        [logOut.fulfilled](state) {
+            state.user = { name: null, email: null };
+            state.token = null;
+            state.isLoggedIn = false;
+            state.isRefreshing = false;
+        }
+    }
+})
+
+export const authReducer = authSlice.reducer;
